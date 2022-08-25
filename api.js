7bc9b6708186550d3fa071c8358cf2ad9ece388f
sebamiranda <carlos.seba.miranda@gmail.com>
2022-08-25 19:38:58 -0300

    api del tiempo

--- api.js
+++ api.js
@@ -1,19 +1,103 @@
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat={-24.1885427}&lon={-65.2738348}&appid={73938b87df2d4be8e19bda6eee85d99d}')
-.then(Response => Response.json())
-.then(json => console.log(json))
+// fetch('https://api.openweathermap.org/data/2.5/weather?lat={-24.1885427}&lon={-65.2738348}&appid={66c91884a6930da951172af85a65d322}')
+// .then(Response => Response.json())
+// .then(json => console.log(json))
 
 
+let temperaturaValor = document.getElementById('temperatura-valor')  
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
+    
+    let ubicacion = document.getElementById('ubicacion')  
+    let iconoAnimado = document.getElementById('icono-animado') 
+
+    let vientoVelocidad = document.getElementById('viento-velocidad') 
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '975af6935emshb4bb60b0dbff540p1d4076jsn7f432dcecfdc',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
+};
+
+fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=-24.1885427&lon=-65.2738348&units=metric&lang=es', options)
+	.then(response => response.json())
+	.then(data => {
+       
+        console.log(data.data[0])
+        let temp = Math.round(data.data[0].temp)
+       // console.log(temp)
+        temperaturaValor.textContent = `${temp} ° C`
+
+        //console.log(data.weather[0].description)
+        let desc = data.data[0].weather.description
+        temperaturaDescripcion.textContent = desc.toUpperCase()
+        ubicacion.textContent = data.city_name
+        
+        vientoVelocidad.textContent = `${data.data[0].wind_spd} m/s`
+        
+        //para iconos estáticos
+        //const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`                     
+        //icono.src = urlIcon
+        //console.log(data.weather[0].icon)
+
+        //para iconos dinámicos
+        console.log(data.weather[0].main)
+        switch (data.weather[0].main) {
+            case 'Thunderstorm with rain':
+              iconoAnimado.src='animated/thunder.svg'
+              console.log('TORMENTA');
+              break;
+            case 'Drizzle':
+              iconoAnimado.src='animated/rainy-2.svg'
+              console.log('LLOVIZNA');
+              break;
+            case 'Light Rain':
+              iconoAnimado.src='animated/rainy-7.svg'
+              console.log('LLUVIA');
+              break;
+            case 'Snow':
+              iconoAnimado.src='animated/snowy-6.svg'
+                console.log('NIEVE');
+              break;                        
+            case 'Clear sky':
+                iconoAnimado.src='animated/day.svg'
+                console.log('LIMPIO');
+              break;
+            case 'Atmosphere':
+              iconoAnimado.src='animated/weather.svg'
+                console.log('ATMOSFERA');
+                break;  
+            case 'Overcast clouds':
+                iconoAnimado.src='animated/cloudy-day-1.svg'
+                console.log('NUBES');
+                break;  
+            default:
+              iconoAnimado.src='animated/cloudy-day-1.svg'
+              console.log('por defecto');
+          }
+
+       
+
+
+    })
+	.catch(err => console.error(err));
+
+
+
+
+
+
+    
 let boton = document.getElementById('enviar');
     
 boton.addEventListener('click', onClick);
 
 function cleanForm(){
-  document.getElementById('user').value = "",
-   document.getElementById('mail').value = "",
-    document.getElementById('phone').value = "",
-    document.getElementById('brief').value = ""
+    let formulario = document.getElementById('formulario');
+        formulario.reset();
 }
 
 function onClick(event) {
